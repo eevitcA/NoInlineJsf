a23789ac7885fea24f8aaca3f97539cf034b8f46
Ced <Ced@192.168.1.4>
2016-04-27 19:25:33 +0200

    first ready

--- testJSF/WebContent/resources/scripts/gulpfile.js
+++ testJSF/WebContent/resources/scripts/gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
-  // place code for your default task here
+	  return gulp.src("noinlinejs.js")
+	  	.pipe(uglify())
+	  	.pipe(gulp.dest("dist"));
+});
+
+gulp.task('compress', function() {
+	  return gulp.src("noinlinejs.js")
+	  	.pipe(uglify())
+	  	.pipe(gulp.dest(""));
 });
